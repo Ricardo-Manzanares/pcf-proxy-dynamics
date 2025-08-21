0e36e0a51aba5665d4b30b41b5fce88434a326f3
Ricardo Manzanares Camargo <r.manzanares@axazure.com>
2025-08-21 12:25:44 +0200

    Add Prefer header to API request for OData annotations

--- src/server.ts
+++ src/server.ts
@@ -57,7 +57,8 @@ async function headers(): Promise<HeadersInit> {
     'Content-Type': 'application/json',
     'OData-MaxVersion': '4.0',
     'OData-Version': '4.0',
-    'Accept': 'application/json'
+    'Accept': 'application/json',
+    'Prefer': 'odata.include-annotations="*"'
   };
 }
 
